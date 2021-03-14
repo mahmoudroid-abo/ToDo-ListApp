f67df002a66776af74a76eef4d58a11e669059dd
maboe <maboelenen555@gmail.com>
2021-03-14 18:33:01 +0200

    add dao&database

---
+++ app/src/main/java/com/mahmoudroid/todolist/data/TaskDataBase.kt
@@ -0,0 +1,12 @@
+package com.mahmoudroid.todolist.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDataBase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+}
